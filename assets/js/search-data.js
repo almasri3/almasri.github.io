b0f5cd49dc1899b3e6d41eb9fe710bd5f73547cd
almasri3 <77461254+almasri3@users.noreply.github.com>
2026-04-29 18:07:50 +0000

    Deploying to gh-pages from @ almasri3/almasri.github.io@4611e60f81ad42366c32502eabe6fcdcccb561e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,12 +451,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-book-project",
-          title: 'Book Project',
-          description: "Varieties of Radicalism",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -496,6 +491,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-book-project",
+          title: 'Book Project',
+          description: "Varieties of Radicalism",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Book%20Project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
