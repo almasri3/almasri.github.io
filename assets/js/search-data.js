c6cefd4b547b2b778377e39811c92fae3159fd85
almasri3 <77461254+almasri3@users.noreply.github.com>
2026-07-09 19:40:45 +0000

    Deploying to gh-pages from @ almasri3/almasri.github.io@897802dfa44abc46508957fb617709725a2cb965 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -142,21 +142,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/almasri.github.io/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/almasri.github.io/assets/pdf/Nasir_Almasri_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6E%61%73%69%72.%61%6C%6D%61%73%72%69@%77%65%6C%6C%65%73%6C%65%79.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -170,14 +163,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=W6wSc98AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
