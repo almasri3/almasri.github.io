c08d473767b6c93f9290ae6315829fdd91d82918
almasri3 <77461254+almasri3@users.noreply.github.com>
2026-07-10 18:03:25 +0000

    Deploying to gh-pages from @ almasri3/almasri.github.io@0a8e3e99c1e85aab8e16e1ee82500cc3fb0f850f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -140,13 +140,6 @@ ninja.data = [{
           window.open("mailto:%6E%61%73%69%72.%61%6C%6D%61%73%72%69@%77%65%6C%6C%65%73%6C%65%79.%65%64%75", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
